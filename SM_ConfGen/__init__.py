de605e257247eca41c74f717b3d90787a5b805b0
Anika <Anika.Friedman@colorado.edu>
2024-11-21 10:50:08 -0700

    Fix installation issues

--- SM_ConfGen/__init__.py
+++ SM_ConfGen/__init__.py
@@ -1,10 +1,9 @@
 """Small molecule conformer generation using TREMD"""
 
-# Add imports here
-from .sm_confgen import *
+# Handle versioneer
+from ._version import get_versions
 
-
-from ._version import *
-
-from . import _version
-__version__ = _version.get_versions()['version']
+versions = get_versions()
+__version__ = versions["version"]
+__git_revision__ = versions["full-revisionid"]
+del get_versions, versions
